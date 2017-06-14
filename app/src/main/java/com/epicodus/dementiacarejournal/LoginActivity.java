3efe0e7237b9601e537837bd8976b33a743edbe6
Grace Stuart <gracerstuart@gmail.com>
2017-06-14 14:51:36 -0700

    add click method to navigate to login activity

--- app/src/main/java/com/epicodus/dementiacarejournal/LoginActivity.java
+++ app/src/main/java/com/epicodus/dementiacarejournal/LoginActivity.java
@@ -34,9 +34,6 @@ import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
-/**
- * A login screen that offers login via email/password.
- */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
     /**
--- app/src/main/java/com/epicodus/dementiacarejournal/MainActivity.java
+++ app/src/main/java/com/epicodus/dementiacarejournal/MainActivity.java
@@ -1,14 +1,16 @@
 package com.epicodus.dementiacarejournal;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
     @Bind (R.id.aboutButton) Button mAboutButton;
     @Bind (R.id.logInButton) Button mLogInButton;
 
@@ -22,5 +24,18 @@ public class MainActivity extends AppCompatActivity {
             mAboutButton.setTypeface(text);
             mLogInButton.setTypeface(text);
 
+        mAboutButton.setOnClickListener(this);
+        mLogInButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mAboutButton) {
+
+        }
+        if (v == mLogInButton) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
