4f1604aa1c161c845e9928a74b8c51d2f10582e2
Grace Stuart <gracerstuart@gmail.com>
2017-06-14 15:39:56 -0700

    add login activity fields

--- app/src/main/java/com/epicodus/dementiacarejournal/LoginActivity.java
+++ app/src/main/java/com/epicodus/dementiacarejournal/LoginActivity.java
@@ -2,12 +2,18 @@ package com.epicodus.dementiacarejournal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.Bind;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.signInButton) Button mSignInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
