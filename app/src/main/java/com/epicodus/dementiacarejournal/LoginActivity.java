ccbcd54613b2efafd16bc6cb3e73ef3ed88060ae
Grace Stuart <gracerstuart@gmail.com>
2017-06-15 11:03:58 -0700

    add onclickListener

--- app/src/main/java/com/epicodus/dementiacarejournal/LoginActivity.java
+++ app/src/main/java/com/epicodus/dementiacarejournal/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -24,6 +25,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
         mSignInButton.setOnClickListener(this);
+        mAboutButton.setOnClickListener(this);
+        mRegisterTextView.setOnClickListener();
     }
 
     @Override
