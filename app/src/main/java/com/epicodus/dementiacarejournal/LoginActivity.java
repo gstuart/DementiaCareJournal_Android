d2af446965505832d7c35c4db1c19879fe2f90e5
Grace Stuart <gracerstuart@gmail.com>
2017-06-15 10:12:56 -0700

    add functionality to login activity

--- app/src/main/java/com/epicodus/dementiacarejournal/LoginActivity.java
+++ app/src/main/java/com/epicodus/dementiacarejournal/LoginActivity.java
@@ -6,13 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-    @Bind(R.id.signInButton) Button mSignInButton;
+    @Bind(R.id.passwordLoginButton) Button mSignInButton;
     @Bind(R.id.aboutButton) Button mAboutButton;
+    @Bind(R.id.registerTextView) TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +29,17 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View v) {
         if (v == mAboutButton) {
-
+            Intent intent = new Intent(LoginActivity.this, AboutActivity.class);
+            startActivity(intent);
         }
         if (v == mSignInButton) {
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
+        }
+        if (v == mRegisterTextView) {
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
         }
 
     }
