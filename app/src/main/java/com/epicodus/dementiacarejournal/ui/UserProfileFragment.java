158a01305d2cbdfadc4b2f79e169db2c22346feb
Grace Stuart <gracerstuart@gmail.com>
2017-06-19 13:57:53 -0700

    add fragments with save button

---
+++ app/src/main/java/com/epicodus/dementiacarejournal/ui/UserProfileFragment.java
@@ -0,0 +1,31 @@
+package com.epicodus.dementiacarejournal.ui;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.epicodus.dementiacarejournal.R;
+
+public class UserProfileFragment extends Fragment implements View.OnClickListener {
+    private Button saveButton;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_user_profile, container, false);
+        saveButton = (Button) view.findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
