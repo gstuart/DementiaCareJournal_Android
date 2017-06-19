18b50735127d406ff4716a7aabcbe0cba34602e1
Grace Stuart <gracerstuart@gmail.com>
2017-06-19 16:42:17 -0700

    add and remove toDo items

--- app/src/main/java/com/epicodus/dementiacarejournal/ui/UserProfileFragment.java
+++ app/src/main/java/com/epicodus/dementiacarejournal/ui/UserProfileFragment.java
@@ -11,13 +11,17 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.epicodus.dementiacarejournal.R;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class UserProfileFragment extends Fragment implements View.OnClickListener {
     private Button saveButton;
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {]
+
+        //TODO add functionality to pull user data to be displayed in the editText fields;
+
         View view = inflater.inflate(R.layout.fragment_user_profile, container, false);
         saveButton = (Button) view.findViewById(R.id.saveButton);
         saveButton.setOnClickListener(this);
