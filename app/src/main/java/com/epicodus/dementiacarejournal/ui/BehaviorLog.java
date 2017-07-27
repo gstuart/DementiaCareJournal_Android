38d0603563e616eaf2227f0edd081e96b0fb8e90
Grace Stuart <gracerstuart@gmail.com>
2017-07-26 18:54:42 -0700

    correct save method and refactored array to listArray

--- app/src/main/java/com/epicodus/dementiacarejournal/ui/BehaviorLog.java
+++ app/src/main/java/com/epicodus/dementiacarejournal/ui/BehaviorLog.java
@@ -3,6 +3,7 @@ package com.epicodus.dementiacarejournal.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.AdapterView;
@@ -13,6 +14,14 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Toast;
 
 import com.epicodus.dementiacarejournal.R;
+import com.epicodus.dementiacarejournal.models.Patient;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -20,26 +29,31 @@ import butterknife.ButterKnife;
 public class BehaviorLog extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.saveButton) Button mSaveButton;
 
+    private FirebaseAuth mAuth;
+    private DatabaseReference mPatient;
+    ArrayList <String> ValueHolder;
+
     ListView listview;
-    String[] ListViewItems = new String[] {
-            "Agitation",
-            "Eating",
-            "Good hygiene",
-            "Hallucinations",
-            "Incoherient",
-            "Incontinence",
-            "Lethargic",
-            "Silent",
-            "Paranoia",
-            "Repetitive speech or actions (Perseveration)",
-            "Self dressing",
-            "Sexually inappropriate behavior",
-            "Shadowing, imitates and follows the caregiver",
-            "Sleeplessness/Sundowning",
-            "Uncooperative and resistant",
-            "Verbal outbursts",
-            "Wandering"
-    };
+    ArrayList<String> ListViewItems = new ArrayList<String>();
+// todo create an array which contains the following :
+//    ListViewItems.add(
+//            "Agitation",
+//            "Eating",
+//            "Good hygiene",
+//            "Hallucinations",
+//            "Incoherient",
+//            "Incontinence",
+//            "Lethargic",
+//            "Silent",
+//            "Paranoia",
+//            "Repetitive speech or actions (Perseveration)",
+//            "Self dressing",
+//            "Sexually inappropriate behavior",
+//            "Shadowing, imitates and follows the caregiver",
+//            "Sleeplessness/Sundowning",
+//            "Uncooperative and resistant",
+//            "Verbal outbursts",
+//            "Wandering");
 
     SparseBooleanArray sparseBooleanArray ;
 
@@ -51,8 +65,26 @@ public class BehaviorLog extends AppCompatActivity implements View.OnClickListen
 
         mSaveButton.setOnClickListener(this);
 
-        listview = (ListView)findViewById(R.id.listView);
+        //  add items to ArrayList ListViewItems
+        ListViewItems.add("Agitation");
+        ListViewItems.add("Eating");
+        ListViewItems.add("Good hygiene");
+        ListViewItems.add("Hallucinations");
+        ListViewItems.add("Incoherient");
+        ListViewItems.add("Incontinence");
+        ListViewItems.add("Silent");
+        ListViewItems.add("Paranoia");
+        ListViewItems.add("Repetitive speech or actions (Perseveration)");
+        ListViewItems.add("Self dressing");
+        ListViewItems.add("Sexually inappropriate behavior");
+        ListViewItems.add("Shadowing, imitates and follows the caregiver");
+        ListViewItems.add("Sleeplessness/Sundowning");
+        ListViewItems.add("Uncooperative and resistant");
+        ListViewItems.add("Verbal outbursts");
+        ListViewItems.add("Wandering");
 
+        listview = (ListView)findViewById(R.id.listView);
+        mAuth = FirebaseAuth.getInstance();
         ArrayAdapter<String> adapter = new ArrayAdapter<String>
                 (BehaviorLog.this,
                         android.R.layout.simple_list_item_multiple_choice,
@@ -64,22 +96,20 @@ public class BehaviorLog extends AppCompatActivity implements View.OnClickListen
         {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // TODO 6/30 Save selected items to the database with a timestamp
+                // TODO 6/30 Save selected items (String[]) to the database with a timestamp
 
                 sparseBooleanArray = listview.getCheckedItemPositions();
 
-                String ValueHolder = "" ;
+                ValueHolder = new ArrayList<String>();
 
                 int i = 0 ;
                 while (i < sparseBooleanArray.size()) {
                     if (sparseBooleanArray.valueAt(i)) {
-
-                        ValueHolder += ListViewItems [ sparseBooleanArray.keyAt(i) ] + ",";
+                        ValueHolder.add(ListViewItems.get(i));
                     }
                     i++ ;
                 }
-
-                ValueHolder = ValueHolder.replaceAll("(,)*$", "");
+//                ValueHolder = ValueHolder.replaceAll("(,)*$", "");
             }
         });
     }
@@ -87,9 +117,22 @@ public class BehaviorLog extends AppCompatActivity implements View.OnClickListen
     @Override
     public void onClick(View v) {
         if (v == mSaveButton) {
-            Intent intent = new Intent(BehaviorLog.this, EmotionLog.class);
+            savePatientBehaviors();
             Toast.makeText(BehaviorLog.this, "Selected Items Logged", Toast.LENGTH_SHORT).show();
-            startActivity(intent);
         }
+    }
+
+    private void savePatientBehaviors() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        DatabaseReference ref = FirebaseDatabase
+                .getInstance()
+                .getReference(user.getUid())
+                .child("patient")
+                .child("behaviors");
+
+        ref.push().setValue(ValueHolder);
+
+        Intent intent = new Intent(BehaviorLog.this, EmotionLog.class);
+        startActivity(intent);
     }
 }
