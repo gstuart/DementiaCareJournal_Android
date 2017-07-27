8e72af7aac37b602ea10ec334ac4903bdb16dd77
Grace Stuart <gracerstuart@gmail.com>
2017-07-27 08:11:29 -0700

    add savePatientEmotions method

--- app/src/main/java/com/epicodus/dementiacarejournal/ui/EmotionLog.java
+++ app/src/main/java/com/epicodus/dementiacarejournal/ui/EmotionLog.java
@@ -3,6 +3,7 @@ package com.epicodus.dementiacarejournal.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.AdapterView;
@@ -13,40 +14,24 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.epicodus.dementiacarejournal.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class EmotionLog extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.saveButton) Button mSaveButton;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mPatient;
+    ArrayList<String> ValueHolder;
 
     ListView listview;
-    String[] ListViewItems = new String[] {
-            "Serenity",
-            "Joy",
-            "Ecstasy",
-            "Acceptance",
-            "Trust",
-            "Admiration",
-            "Apprehension",
-            "Fear",
-            "Terror",
-            "Distration",
-            "Surprise",
-            "Amazement",
-            "Pensiveness",
-            "Sadness",
-            "Grief",
-            "Boredom",
-            "Disgust",
-            "Loathing",
-            "Annoyance",
-            "Anger",
-            "Rage",
-            "Interest",
-            "Anticipation",
-            "Vigilance"
-    };
+    ArrayList<String> ListViewItems = new ArrayList<String>();
 
     SparseBooleanArray sparseBooleanArray ;
 
@@ -57,37 +42,57 @@ public class EmotionLog extends AppCompatActivity implements View.OnClickListene
         ButterKnife.bind(this);
         mSaveButton.setOnClickListener(this);
 
+        ListViewItems.add("Serenity");
+        ListViewItems.add("Joy");
+        ListViewItems.add("Ecstasy");
+        ListViewItems.add("Acceptance");
+        ListViewItems.add("Trust");
+        ListViewItems.add("Admiration");
+        ListViewItems.add("Apprehension");
+        ListViewItems.add("Fear");
+        ListViewItems.add("Terror");
+        ListViewItems.add("Distration");
+        ListViewItems.add("Surprise");
+        ListViewItems.add("Amazement");
+        ListViewItems.add("Pensiveness");
+        ListViewItems.add("Sadness");
+        ListViewItems.add("Grief");
+        ListViewItems.add("Boredom");
+        ListViewItems.add("Disgust");
+        ListViewItems.add("Loathing");
+        ListViewItems.add("Annoyance");
+        ListViewItems.add("Anger");
+        ListViewItems.add("Rage");
+        ListViewItems.add("Interest");
+        ListViewItems.add("Anticipation");
+        ListViewItems.add("Vigilance");
+        ListViewItems.add("Disgust");
 
-        listview = (ListView)findViewById(R.id.listView);
-
+        listview = (ListView) findViewById(R.id.listView);
+        mAuth = FirebaseAuth.getInstance();
         ArrayAdapter<String> adapter = new ArrayAdapter<String>
                 (EmotionLog.this,
                         android.R.layout.simple_list_item_multiple_choice,
-                        android.R.id.text1, ListViewItems );
+                        android.R.id.text1, ListViewItems);
 
         listview.setAdapter(adapter);
 
-        listview.setOnItemClickListener(new OnItemClickListener()
-        {
+        listview.setOnItemClickListener(new OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // TODO 6/30 Save selected items to the database with a timestamp
-
                 sparseBooleanArray = listview.getCheckedItemPositions();
 
-                String ValueHolder = "" ;
+                ValueHolder = new ArrayList<String>();
 
-                int i = 0 ;
+                int i = 0;
                 while (i < sparseBooleanArray.size()) {
                     if (sparseBooleanArray.valueAt(i)) {
-
-                        ValueHolder += ListViewItems [ sparseBooleanArray.keyAt(i) ] + ",";
+                        ValueHolder.add(ListViewItems.get(i));
                     }
-                    i++ ;
+                    i++;
                 }
-
-                ValueHolder = ValueHolder.replaceAll("(,)*$", "");
-
+//                ValueHolder = ValueHolder.replaceAll("(,)*$", "");
+                Log.i("Array contents", ValueHolder.toString());
             }
         });
     }
@@ -95,9 +100,23 @@ public class EmotionLog extends AppCompatActivity implements View.OnClickListene
     @Override
     public void onClick(View v) {
         if (v == mSaveButton) {
-            Intent intent = new Intent(EmotionLog.this, ActivityLog.class);
+            savePatientEmotions();
             Toast.makeText(EmotionLog.this, "Selected Items Logged", Toast.LENGTH_SHORT).show();
-            startActivity(intent);
         }
+    }
+
+    private void savePatientEmotions() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        DatabaseReference ref = FirebaseDatabase
+                .getInstance()
+                .getReference(user.getUid())
+                .child("patient")
+                .child("behaviors");
+
+        ref.push().setValue(ValueHolder);
+        Log.i("  SAVE METHOD    ", ValueHolder.toString());
+
+        Intent intent = new Intent(EmotionLog.this, ActivityLog.class);
+        startActivity(intent);
     }
 }
